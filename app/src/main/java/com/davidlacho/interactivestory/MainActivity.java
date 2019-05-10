47a54198c4b2076ba6c609f0a6c1b9bc4ec2e86c
davidlacho <david.lacho@qhrtech.com>
2019-05-10 16:05:23 -0700

    first commit

---
+++ app/src/main/java/com/davidlacho/interactivestory/MainActivity.java
@@ -0,0 +1,42 @@
+package com.davidlacho.interactivestory;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+  private EditText nameField;
+  private Button startButton;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    nameField = findViewById(R.id.nameEditText);
+    startButton = findViewById(R.id.startButton);
+
+    startButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String name = nameField.getText().toString();
+//        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+        startStory(name);
+      }
+    });
+  }
+
+  private void startStory(String name) {
+    Intent intent = new Intent(this, StoryActivity.class);
+    Resources resources = getResources();
+    intent.putExtra(resources.getString(R.string.key_name), name);
+    startActivity(intent);
+  }
+}
